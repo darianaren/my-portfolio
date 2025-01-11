d449234e0b566064989e33ae7cc78d36aa7db522
Dariana Rengifo <darianarengifo@gmail.com>
2025-01-11 17:31:02 -0300

    [main] feat (components/Background)

---
+++ src/components/Background/constants.js
@@ -0,0 +1,31 @@
+export const VARIABLES_BG_SHAPE_1 = Object.freeze({
+  top: "10vh",
+  left: "75vw",
+  width: "15vw",
+  height: "15vw"
+});
+
+export const VARIABLES_BG_SHAPE_2 = Object.freeze({
+  top: "85vh",
+  left: "50vw",
+  width: "20vw",
+  height: "20vw"
+});
+export const VARIABLES_BG_SHAPE_3 = Object.freeze({
+  top: "70vh",
+  left: "10vw",
+  width: "25vw",
+  height: "25vw"
+});
+export const VARIABLES_BG_SHAPE_4 = Object.freeze({
+  top: "-7vw",
+  left: "-7vw",
+  width: "30vw",
+  height: "30vw"
+});
+export const VARIABLES_BG_SHAPE_5 = Object.freeze({
+  top: "60vh",
+  left: "70vw",
+  width: "35vw",
+  height: "35vw"
+});
