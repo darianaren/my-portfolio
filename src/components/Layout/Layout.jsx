de3cddb49213028a7ae5bdfff74cac112e3f7693
Dariana Rengifo <darianarengifo@gmail.com>
2025-02-23 13:54:46 -0300

    [main] feat (src/components/Layout)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+
+import styles from "./styles.module.css";
+
 import NavBar from "../NavBar/NavBar";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <NavBar />
-
       <main>{children}</main>
     </div>
   );
